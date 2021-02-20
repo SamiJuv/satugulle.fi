ae3a94cbe05dc6b3e0fc62b1d04fc162c2a4a23f
Sami Juvonen <sami.juvonen@karhuhelsinki.fi>
2021-02-20 13:39:38 +0200

    Filename fix

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Img from 'gatsby-image'
 import styled from 'styled-components'
 
-import Menu from './Menu';
+import Menu from './menu';
 import LogoImg from '../images/logo.svg'
 
 const Container = styled.div`
